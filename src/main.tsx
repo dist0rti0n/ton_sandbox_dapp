2ee17006336dc26296abb133df5360e84f0b57ca
distortion <distortion.says@gmail.com>
2025-07-07 01:44:10 +0500

    Left crutch in manifest, add TODO

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import './index.css'
 import App from './App.tsx'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
+// TODO: figure out the problem with my icon in public/tonconnect-manifest.json 
 //const manifestUrl = "https://api.jsonsilo.com/demo/715785d1-4198-4b21-a4c2-9e8993e3b665"
 // const manifestUrl = "https://api.jsonsilo.com/demo/38aead0a-7b9a-44b5-902b-9a78eafb4d6a"
  const manifestUrl = "https://dist0rti0n.github.io/ton_sandbox_dapp/tonconnect-manifest.json";
