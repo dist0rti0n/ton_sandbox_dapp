0b5fa78a074ec07b938fca947bfe9913f1846ec5
distortion <distortion.says@gmail.com>
2025-07-07 01:59:10 +0500

    Fix manifest

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import App from './App.tsx'
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 // TODO: figure out the problem with my icon in public/tonconnect-manifest.json 
-//const manifestUrl = "https://api.jsonsilo.com/demo/715785d1-4198-4b21-a4c2-9e8993e3b665"
+// const manifestUrl = "https://api.jsonsilo.com/demo/d167cdd9-945a-492e-8118-0d4e7f1d3d50"
+// const manifestUrl = "https://api.jsonsilo.com/demo/4ec761b9-80cb-40a6-b165-09141632ab55"
+// const manifestUrl = "https://api.jsonsilo.com/demo/715785d1-4198-4b21-a4c2-9e8993e3b665"
 // const manifestUrl = "https://api.jsonsilo.com/demo/38aead0a-7b9a-44b5-902b-9a78eafb4d6a"
- const manifestUrl = "https://dist0rti0n.github.io/ton_sandbox_dapp/tonconnect-manifest.json";
-// const manifestUrl = "https://raw.githubusercontent.com/markokhman/func-course-chapter-5-code/master/public/manifest.json";
+ const manifestUrl = "https://dist0rti0n.github.io/ton_sandbox_dapp/tonconnect-manifest.json"
+// const manifestUrl = "https://raw.githubusercontent.com/markokhman/func-course-chapter-5-code/master/public/manifest.json"
 
 createRoot(document.getElementById('root')!).render(
 <TonConnectUIProvider manifestUrl={manifestUrl}>
